c80c769f6964aafffc7d14c2d872c5a620844f88
Jerry Wang <jerry@Jerrys-MacBook-Pro.local>
2019-08-27 17:39:46 -0700

    Intitializing

--- aliexpressScraper.js
+++ aliexpressScraper.js
@@ -1,32 +1,40 @@
 const puppeteer = require("puppeteer");
-const random = require("random-name");
 const dayjs = require("dayjs");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const _ = require("lodash");
-const getRandomLocation = require("./cities");
+
 const LanguageDetect = require("languagedetect");
 const lngDetector = new LanguageDetect();
 
-const getAliExpressFiveStarReviews = async (url, csvWriter) => {
+/**
+ *
+ *
+ * @param {String} url = ali-express url to scrape
+ * @param {Object} csvWriter = csv-writer instance
+ * @param {Object} options = {translate, englishOnly, reviewType}
+ */
+
+const getAliExpressReviews = async (url, csvWriter, options) => {
+  // Open the browser and go to the given url
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  // await page.setViewport({
-  //   width: 1920,
-  //   height: 1080
-  // });
+
   await page.goto(url);
 
-  await page.waitFor("#cb-translate");
-  await page.click("#cb-translate");
+  if (options.translate) {
+    // Uncheck on the "translate" button, which automatically translates non-english reviews
+    await page.waitFor("#cb-translate");
+    await page.click("#cb-translate");
+  }
 
+  // Grab only the reviews for the given reviewType
   await page.waitFor(".fb-star-list-href");
   const reviewButtons = await page.$$(".fb-star-list-href");
-  const fiveStarReviews = reviewButtons[0];
+  const reviewsButton = reviewButtons[5 - options.reviewType];
 
-  console.log("Now grabbing the five star reviews...");
+  await reviewsButton.click();
 
-  await fiveStarReviews.click();
-
+  // Keep track of how many pages of reviews for the given reviewType
   await page.waitFor(".fb-star-selector");
   const reviewCount = Number(
     (await page.evaluate(
@@ -36,8 +44,9 @@ const getAliExpressFiveStarReviews = async (url, csvWriter) => {
       .slice(1)
       .slice(0, -1)
   );
-  const fiveStarReviewPages = Math.floor(reviewCount / 10);
+  const reviewPages = Math.floor(reviewCount / 10);
 
+  // Scrape all the reviews on the first page
   const reviews = await page.evaluate(() =>
     [...document.querySelectorAll(".buyer-feedback:not(.r-time-new)")].map(
       el => ({
@@ -47,12 +56,10 @@ const getAliExpressFiveStarReviews = async (url, csvWriter) => {
     )
   );
 
+  // Get the reviews for the rest of the pages
   (async function() {
-    console.log(
-      `Scraping the other ${fiveStarReviewPages - 1} five star review pages...`
-    );
-
-    for (let i = 2; i < fiveStarReviewPages; i++) {
+    for (let i = 2; i < reviewPages; i++) {
+      // Navigate to the next page
       await page.waitFor("a.ui-pagination-next.ui-goto-page");
       const nextButtonData = await page.$$("a.ui-pagination-next.ui-goto-page");
       const nextButton = [...nextButtonData][1];
@@ -61,6 +68,7 @@ const getAliExpressFiveStarReviews = async (url, csvWriter) => {
       await page.waitFor(".buyer-feedback");
       await page.waitFor(".buyer-feedback > span");
 
+      // Scrape all the reviews on this page
       const reviewsFromPage = await page.evaluate(() =>
         [...document.querySelectorAll(".buyer-feedback")].map(el => ({
           review: el.querySelector("span").innerText.trim(),
@@ -68,184 +76,69 @@ const getAliExpressFiveStarReviews = async (url, csvWriter) => {
         }))
       );
 
+      // Push the scraped reviews into the original reviews array
       reviews.push(reviewsFromPage);
     }
 
-    console.log(
-      `Got all ${
-        reviews.length
-      } five star reviews. Now filtering out the non-english ones...`
-    );
-    const englishReviews = _.flatten(reviews).filter(({ review }) => {
-      const language = lngDetector.detect(review, 1);
-      return !_.isEmpty(language) ? language[0][0] === "english" : false;
-    });
+    // All reviews for the given reviewType has been scraped...
 
-    console.log(
-      `${
-        englishReviews.length
-      } five star reviews left. Now formatting for CSV...`
-    );
-    const formattedReviews = englishReviews.map(el => {
-      const first = random.first();
-      const last = random.last();
+    let formattedReviews;
+    if (options.translate) {
+      // Filter out the non-english reviews
+      const englishReviews = _.flatten(reviews).filter(({ review }) => {
+        const language = lngDetector.detect(review, 1);
+        return !_.isEmpty(language) ? language[0][0] === "english" : false;
+      });
 
-      return {
-        product_handle: null,
-        state: "published",
-        rating: 5,
-        title: null,
-        author: `${first} ${last.charAt(0)}.`,
-        email: `${_.lowerFirst(first)}${_.lowerFirst(last)}${Math.floor(
-          Math.random() * 121
-        )}@gmail.com`,
-        location: getRandomLocation(),
+      // Format the reviews for the CSV file
+      formattedReviews = englishReviews.map(el => ({
+        rating: reviewType,
         body: el.review,
-        reply: null,
-        created_at: dayjs(el.date).format("YYYY-MM-DD"),
-        replied_at: null
-      };
-    });
-
-    // console.log(formattedReviews);
-    // console.log(formattedReviews.length);
-
-    console.log("Writing five star reviews to file...");
-    await csvWriter.writeRecords(formattedReviews).catch(e => console.log(e));
-
-    console.log("Finished!");
-  })();
-};
-
-const getAliExpressFourStarReviews = async (url, csvWriter) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  // await page.setViewport({
-  //   width: 1920,
-  //   height: 1080
-  // });
-  await page.goto(url);
-
-  await page.waitFor("#cb-translate");
-  await page.click("#cb-translate");
-
-  await page.waitFor(".fb-star-list-href");
-  const reviewButtons = await page.$$(".fb-star-list-href");
-  const fourStarReviews = reviewButtons[1];
-
-  console.log("Now grabbing the four star reviews...");
-
-  await fourStarReviews.click();
-
-  await page.waitFor(".fb-star-selector");
-  const reviewCount = Number(
-    (await page.evaluate(
-      () => document.querySelector(".fb-star-selector").innerText
-    ))
-      .split(" ")[2]
-      .slice(1)
-      .slice(0, -1)
-  );
-  const fourStarReviewPages = Math.floor(reviewCount / 10);
-
-  const reviews = await page.evaluate(() =>
-    [...document.querySelectorAll(".buyer-feedback:not(.r-time-new)")].map(
-      el => ({
-        review: el.querySelector("span").innerText.trim(),
-        date: el.querySelectorAll("span")[1].innerText.trim()
-      })
-    )
-  );
-
-  (async function() {
-    console.log(`Scraping the other ${fourStarReviewPages - 1} pages...`);
-    for (let i = 2; i < fourStarReviewPages; i++) {
-      await page.waitFor("a.ui-pagination-next.ui-goto-page");
-      const nextButtonData = await page.$$("a.ui-pagination-next.ui-goto-page");
-      const nextButton = [...nextButtonData][1];
-
-      await nextButton.click();
-      await page.waitFor(".buyer-feedback");
-      await page.waitFor(".buyer-feedback > span");
-
-      const reviewsFromPage = await page.evaluate(() =>
-        [...document.querySelectorAll(".buyer-feedback")].map(el => ({
-          review: el.querySelector("span").innerText.trim(),
-          date: el.querySelectorAll("span")[1].innerText.trim()
-        }))
-      );
-
-      reviews.push(reviewsFromPage);
+        created_at: dayjs(el.date).format("YYYY-MM-DD")
+      }));
+    } else {
+      formattedReviews = _.flatten(reviews).map(el => ({
+        rating: reviewType,
+        body: el.review,
+        created_at: dayjs(el.date).format("YYYY-MM-DD")
+      }));
     }
 
-    console.log(
-      `Got all ${
-        reviews.length
-      } reviews. Now filtering out the non-english ones...`
-    );
-    const englishReviews = _.flatten(reviews).filter(({ review }) => {
-      const language = lngDetector.detect(review, 1);
-      return !_.isEmpty(language) ? language[0][0] === "english" : false;
-    });
-
-    console.log(
-      `${englishReviews.length} reviews left. Now formatting for CSV...`
-    );
-    const formattedReviews = englishReviews.map(el => {
-      const first = random.first();
-      const last = random.last();
-
-      return {
-        product_handle: null,
-        state: "published",
-        rating: 5,
-        title: null,
-        author: `${first} ${last.charAt(0)}.`,
-        email: `${_.lowerFirst(first)}${_.lowerFirst(last)}${Math.floor(
-          Math.random() * 121
-        )}@gmail.com`,
-        location: getRandomLocation(),
-        body: el.review,
-        reply: null,
-        created_at: dayjs(el.date).format("YYYY-MM-DD"),
-        replied_at: null
-      };
-    });
-
-    // console.log(formattedReviews);
-    // console.log(formattedReviews.length);
-
-    console.log("Writing four star reviews to file...");
-
-    await csvWriter.writeRecords(formattedReviews).catch(e => console.log(e));
-
-    console.log("Finished!");
+    // Write formatted reviews to the CSV file
+    return csvWriter.writeRecords(formattedReviews).catch(e => console.log(e));
   })();
 };
 
-const getAliExpressReviews = (
+/**
+ *
+ *
+ * @param {String} url = aliexpress url to scrape
+ * @param {String} filename = csv file name to save reviews to
+ * @param {Object} options = {translate, englishOnly, reviewTypes}
+ */
+
+const getAllAliExpressReviews = (
   url,
-  filename = `results-${dayjs().format("YYYY-MM-DD")}`
+  filename = `results-${dayjs().format("YYYY-MM-DD")}`,
+  options = {
+    reviewTypes: [1, 2, 3, 4, 5],
+    translate: false,
+    englishOnly: true
+  }
 ) => {
   const csvWriter = createCsvWriter({
     path: `${filename}.csv`,
     append: true,
     header: [
-      { id: "product_handle", title: "product_handle" },
-      { id: "state", title: "state" },
       { id: "rating", title: "rating" },
-      { id: "title", title: "title" },
-      { id: "author", title: "author" },
-      { id: "email", title: "email" },
-      { id: "location", title: "location" },
       { id: "body", title: "body" },
-      { id: "reply", title: "reply" },
-      { id: "created_at", title: "created_at" },
-      { id: "replied_at", title: "replied_at" }
+      { id: "created_at", title: "created_at" }
     ]
   });
-  getAliExpressFiveStarReviews("https:" + url, csvWriter);
-  getAliExpressFourStarReviews("https:" + url, csvWriter);
+
+  reviewTypes.forEach(reviewType =>
+    getAliExpressReviews("https:" + url, csvWriter, { ...options, reviewType })
+  );
 };
 
-module.export = getAliExpressReviews;
+module.exports = getAllAliExpressReviews;
--- index.js
+++ index.js
@@ -1,73 +1,14 @@
-const getAliExpressReviews = require("./aliexpressScraper");
+const getAllAliExpressReviews = require("./aliexpressScraper");
 
-getAliExpressReviews(
+getAllAliExpressReviews(
   "INSERT URL HERE", // eg: "//feedback.aliexpress.com/display/productEvaluation"
   "INSERT FILE NAME HERE" // eg: transparent_glasses
 );
 
-// Previous examples:
-
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32851172873&ownerMemberId=201744755&companyId=214317891&memberType=seller&startValidDate=&i18n=true",
-//   "transparent_glasses"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32987940193&ownerMemberId=234579237&companyId=243770399&memberType=seller&startValidDate=&i18n=true",
-//   "retro_round_pink"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32811157529&ownerMemberId=230474934&companyId=239958899&memberType=seller&startValidDate=&i18n=true",
-//   "cat_eye_metal_reflective"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32922146070&ownerMemberId=231397422&companyId=240737508&memberType=seller&startValidDate=&i18n=true",
-//   "cat_eye_shade_vintage_retro"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32868313784&ownerMemberId=230805108&companyId=240214190&memberType=seller&startValidDate=&i18n=true",
-//   "new_fashion_sunglasses_feamel_oculos"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32847961047&ownerMemberId=231184420&companyId=240697201&memberType=seller&startValidDate=&i18n=true",
-//   "Vintage Sunglasses Women Cat Eye Luxury Brand Designer Sun Glasses Retro Small Red ladies Sunglass Black Eyewear oculos"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32972385695&ownerMemberId=230823973&companyId=240220146&memberType=seller&startValidDate=&i18n=true",
-//   "kim kardashian sunglasses woman vintage retro flat top Thin Shadow sun glasses square Pilot luxury designer large black shades"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32854874692&ownerMemberId=230823973&companyId=240220146&memberType=seller&startValidDate=&i18n=true",
-//   "Small Oval Sunglasses for men Male retro Metal frame yellow red vintage small round sun glasses for women 2019"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=33006069899&ownerMemberId=237324621&companyId=245393311&memberType=seller&startValidDate=&i18n=true",
-//   "New Fashion Women Flat Sunglasses Luxury Brand Designer Sun glasses Eyewear Candy Color Mirror UV400 oculos de sol"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32869425569&ownerMemberId=232019042&companyId=241245159&memberType=seller&startValidDate=&i18n=true",
-//   "YOOSKE Cute Sexy Cat Eye Sunglasses Women Retro Small Black Red Pink Cateye Sun Glasses Female Vintage Shades for Women"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32885216198&ownerMemberId=233012327&companyId=242218209&memberType=seller&startValidDate=&i18n=true",
-//   "2018 Vintage Small Narrow Sunglasses For Women Men Brand Rimless Rectangle Sun Glasses Retro Metal Frame Clear Lens Red Shades"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32967760829&ownerMemberId=232972533&companyId=242216181&memberType=seller&startValidDate=&i18n=true",
-//   "BAOLANG Luxury Cat eye Sunglasses Women Brand Design Colorful Metal Mirror Fashion Female Eyewear UV400 Sun glasses"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32867829679&ownerMemberId=228220423&companyId=237780683&memberType=seller&startValidDate=&i18n=true",
-//   "Retro Black Round Sunglasses for Women Men Small Oval Alloy Frame Summer Style Unisex Sun Glasses Female Male Goggle"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32793606071&ownerMemberId=201744755&companyId=214317891&memberType=seller&startValidDate=&i18n=true",
-//   "2018 New Designer Woman Glasses Optical Frames Metal Round Glasses Frame Clear lens Eyeware Black Silver Gold Eye Glass"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32882213718&ownerMemberId=230326306&companyId=239793144&memberType=seller&startValidDate=&i18n=true",
-//   "Cat Eye Sunglasses Vintage Brand Designer Rose Gold Mirror Sunglasses For Women Metal Reflective Sun Glasses Female Oculos"
-// );
-// getAliExpressReviews(
-//   "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32956424119&ownerMemberId=205620710&companyId=218048050&memberType=seller&startValidDate=&i18n=true",
-//   "Retro Small Cat Eye Sunglasses For Women Brand Designer Metal Half Frame Shade Triangle Eyeglasses Vintage Cateye Sun Glasses"
-// );
+/*
+Example function call:
+  getAllAliExpressReviews(
+    "//feedback.aliexpress.com/display/productEvaluation.htm?v=2&productId=32851172873&ownerMemberId=201744755&companyId=214317891&memberType=seller&startValidDate=&i18n=true",
+    "transparent_glasses"
+  );
+*/
